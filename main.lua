785cb264c5a82d4b54dea0e5f5ace2dfc842fc0a
IDKdir <jysinok@gmail.com>
2025-01-18 11:48:04 -0500

    Hiya

--- main.lua
+++ main.lua
@@ -2,4 +2,18 @@ if not import then
     return
 end
 
-import('/hooks/network.lua')
+local network
+
+for i, v in next, getgc(true) do
+    if typeof(v) == 'table' then
+        if rawget(v, 'send') and rawget(v, 'receive') then
+            network = v
+        end
+    end
+end
+
+if not network then
+    return error('Hook network failed')
+end
+
+getgenv().network = network
